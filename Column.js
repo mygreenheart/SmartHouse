5ca8b3c148e0d95eaf32870e2b0447f0ef2d1d4a
Aleksandr Okhotnycov <sashaohotni26@gmail.com>
2019-10-17 15:41:27 +0300

    17/10/2019

--- Column.js
+++ Column.js
@@ -1,77 +1,82 @@
 "use strict"
 
-
 function Column(name, mode) {
     this._name = name;
     this._mode = mode;
     this._volume = 2;
     this.playlist = ["The Beatles - Let it be", "Eminem - Rap god", "Iggy Pop - Passenger", "Queen - Bohemian Rhapsody", "Ляпис Трубецкой - Евпатория"]
     this._currentSong = this.playlist[0];
-  
+
 }
-Column.prototype.getName = function(){
+Column.prototype.getName = function () {
     return this._name;
 };
 
 
 
-Column.prototype.chooseMode = function(mode){
-    if(mode = "aux"){
+Column.prototype.chooseMode = function (mode) {
+    if (mode = "aux") {
         this._mode = "aux"
-       } else if(mode = "bluetooth"){
+    } else if (mode = "bluetooth") {
         this._mode = "bluethooth";
-       }else if(mode = "radio"){
+    } else if (mode = "radio") {
         this._mode = "radio";
-       }else throw "Ввели неправильное значени, попробуйте выбрать из [AUX, BLUETOOTH, RADIO]"
+    } else throw "Ввели неправильное значени, попробуйте выбрать из [AUX, BLUETOOTH, RADIO]"
 };
 
-Column.prototype.getMode = function(){
+Column.prototype.getMode = function () {
     return this._mode;
 }
 
-Column.prototype.volumeUp = function(){
-    if(this._volume < 100){
-        this._volume =  this._volume+2;
+Column.prototype.volumeUp = function () {
+    if (this._volume < 100) {
+        this._volume = this._volume + 2;
     }
     else throw "Громкость выходит за пределы доступного";
 
 }
 
-Column.prototype.volumeDown = function(){
-    if(this._volume > 0 ){
-        this._volume =  this._volume-2;
+Column.prototype.volumeDown = function () {
+    if (this._volume > 0) {
+        this._volume = this._volume - 2;
     }
     else throw "Громкость выходит за пределы доступного";
 
 }
 
-Column.prototype.getVolume = function(){
+Column.prototype.getVolume = function () {
     return this._volume;
-}    
+}
 
-Column.prototype.getPlaylist = function(){
+Column.prototype.getPlaylist = function () {
     return this.playlist;
 }
 
-Column.prototype.getSong = function(){
+Column.prototype.getSong = function () {
     return this._currentSong;
 }
 
 
-Column.prototype.chooseSong = function(songName){
-    if(this.playlist.indexOf(songName) > 0){
+Column.prototype.chooseSong = function (songName) {
+    if (this.playlist.indexOf(songName) > 0) {
         this._currentSong = songName;
     }
-     else throw "Exeption"
+    else throw "Exeption"
 }
 
-Column.prototype.playSong = function(){
-    currentSong = this.playlist[Math.floor(Math.random()*this.playlist.length)];//Возвращает рандомный элемент массива playlist
+Column.prototype.playSong = function () {
+    return console.log("Сейчас играет", this._currentSong);
+
+}
+
+Column.prototype.playRandomSong = function () {
+    currentSong = this.playlist[Math.floor(Math.random() * this.playlist.length)];//Возвращает рандомный элемент массива playlist
     this._currentSong = currentSong;
-    return console.log("Сейчас играет",currentSong);
- 
+    return console.log("Сейчас играет", currentSong);
+
 }
 
 
 
-var jbl = new Column("JBL","aux",6);
+
+var jbl = new Column("JBL", "aux", 6);
